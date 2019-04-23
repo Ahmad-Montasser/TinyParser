9fbb60c22a8e1b654343bde4e6600a6812a38039
Ahmad-Montasser <41066332+Ahmad-Montasser@users.noreply.github.com>
2019-04-23 07:15:47 +0200

    Bug Fixes

--- src/Parser.java
+++ src/Parser.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 public class Parser {
     public static int index=0;
     private ArrayList<Token> tokenList ;
-
-
     public Parser(ArrayList<Token> tokenList) {
         this.tokenList = tokenList;
         program();
@@ -13,7 +11,7 @@ public class Parser {
     private void matchValue(Token expectedToken){
         Token nextToken = tokenList.get(index);
             if(nextToken.equals(expectedToken)){
-                System.out.println(nextToken);
+           //     System.out.println(nextToken);
                 index++;
             }
 
@@ -21,10 +19,152 @@ public class Parser {
     private void matchType(Token expectedToken) {
         Token nextToken = tokenList.get(index);
         if (expectedToken.getType().equalsIgnoreCase(expectedToken.getType())){
-            System.out.println(nextToken);
+        //  System.out.println(nextToken);
             index++;
         }
+    }
 
+
+
+    private void program() {
+        System.out.println("program is Found");
+        stmtSequence();
+    }
+
+    private void stmtSequence() {
+        System.out.println("stmt-seq is Found");
+        statement();
+        while(tokenList.size()>index&&tokenList.get(index).getValue().equalsIgnoreCase(";")){
+            matchValue(new Token(";"));
+            //System.out.println("Statement End");
+            if(tokenList.get(index).getValue().equalsIgnoreCase("until")||
+                    tokenList.get(index).getValue().equalsIgnoreCase("end")
+                    )
+                return;
+            statement();
+        }
+
+    }
+
+    private void statement() {
+        if(tokenList.get(index).getValue().equals("read"))
+            readStmt();
+        else if(tokenList.get(index).getValue().equals("write"))
+            writeStmt();
+        else if(tokenList.get(index).getValue().equals("repeat"))
+            repeatStmt();
+        else if(tokenList.get(index).getValue().equals("if"))
+            ifStmt();
+        else if(tokenList.get(index).getType().equals("inId"))
+            assignStmt();
+        else
+            ;//TODO Handle Errors
+
+    }
+
+    private void ifStmt(){
+        System.out.println("ifStmt is Found");
+        matchValue(new Token("if"));
+        matchValue(new Token("("));
+        exp();
+        matchValue(new Token(")"));
+        matchValue(new Token("then"));
+        stmtSequence();
+        if(tokenList.get(index).getValue().equals("else")){
+            matchValue(new Token("else"));
+            stmtSequence();
+        }
+        matchValue(new Token("end"));
+    }
+
+    private void repeatStmt(){
+        System.out.println("repeat is Found");
+        matchValue(new Token("repeat"));
+        stmtSequence();
+        matchValue(new Token("until"));
+        exp();
+    }
+
+    private void assignStmt(){
+        System.out.println("assign is Found");
+        matchType(new Token("inId",""));
+        matchValue(new Token(":="));
+        exp();
+    }
+
+    private void readStmt(){
+        System.out.println("read is Found");
+        matchValue(new Token("read"));
+        matchType(new Token("inId",""));
+
+    }
+
+    private void writeStmt(){
+        System.out.println("write is Found");
+        matchValue(new Token("write"));
+        exp();
+
+    }
+
+    private void exp() {
+        System.out.println("exp is Found");
+        simpleExp();
+        while(tokenList.get(index).getValue().equals("<")||
+                tokenList.get(index).getValue().equals("=")){
+            compOp();
+            simpleExp();
+        }
+
+    }
+
+    private void compOp() {
+        if(tokenList.get(index).getValue().equals("="))
+            matchValue(new Token("="));
+        else if(tokenList.get(index).getValue().equals("<"))
+            matchValue(new Token("<"));
+        else
+            //TODO Handle Errors
+            return;
+
+    }
+
+    private void simpleExp() {
+        term();
+        while(tokenList.get(index).getValue().equals("+")||
+                tokenList.get(index).getValue().equals("-")){
+            addOp();
+            term();
+        }
+
+    }
+
+    private void addOp() {
+        if(tokenList.get(index).getValue().equals("+"))
+            matchValue(new Token("+"));
+        else if(tokenList.get(index).getValue().equals("-"))
+            matchValue(new Token("-"));
+        else
+            //TODO Handle Errors
+            return;
+    }
+
+    private void term() {
+        factor();
+        while(tokenList.get(index).getValue().equals("/")||
+                tokenList.get(index).getValue().equals("*")){
+            mulOp();
+            factor();
+        }
+    }
+
+    private void mulOp() {
+        if(tokenList.get(index).getValue().equals("*"))
+            matchValue(new Token("*"));
+        else if(tokenList.get(index).getValue().equals("/"))
+            matchValue(new Token("/"));
+        else
+            //TODO Handle Errors
+            return;
     }
 
     private void factor(){
@@ -41,126 +181,7 @@ public class Parser {
         }
         else
             //TODO handle errors
-            return;
-    }
-    private void ifStmt(){
-        matchValue(new Token("if"));
-        matchValue(new Token("("));
-        exp();
-        matchValue(new Token(")"));
-        matchValue(new Token("then"));
-        stmtSequence();
-        if(tokenList.get(index).getValue().equals("else")){
-            matchValue(new Token("else"));
-            stmtSequence();
-        }
-        matchValue(new Token("end"));
-    }
-    private void repeatStmt(){
-        matchValue(new Token("repeat"));
-        stmtSequence();
-        matchValue(new Token("until"));
-        exp();
-    }
-    private void assignStmt(){
-        matchType(new Token("inId",""));
-        matchValue(new Token(":="));
-        exp();
-    }
-    private void readStmt(){
-        matchValue(new Token("read"));
-        matchType(new Token("inId",""));
-
-    }
-    private void writeStmt(){
-        matchValue(new Token("write"));
-        exp();
-    }
-    private void statement() {
-        if(tokenList.get(index).getValue().equals("read"))
-            readStmt();
-        else if(tokenList.get(index).getValue().equals("write"))
-            writeStmt();
-        else if(tokenList.get(index).getValue().equals("repeat"))
-            repeatStmt();
-        else if(tokenList.get(index).getValue().equals("if"))
-            ifStmt();
-        else if(tokenList.get(index).getType().equals("inId"))
-            assignStmt();
-        else
-            ;//TODO Handle Errors
-
-    }
-    private void program() {
-        stmtSequence();
-    }
-    private void stmtSequence() {
-        statement();
-        matchValue(new Token(";"));
-        System.out.println("Statment End");
-        while(tokenList.size()>index){
-            matchValue(new Token(";"));
-            statement();
-            System.out.println("Statment End");
-        }
-
+            System.out.println("no match");
     }
 
-    private void exp() {
-        simpleExp();
-        while(tokenList.get(index).getValue().equals("<")||
-                tokenList.get(index).getValue().equals("=")){
-            compOp();
-            matchValue(tokenList.get(index));
-            simpleExp();
-        }
-    }
-
-    private void simpleExp() {
-        term();
-        while(tokenList.get(index).getValue().equals("+")||
-                tokenList.get(index).getValue().equals("-")){
-            addOp();
-            matchValue(tokenList.get(index));
-            term();
-        }
-    }
-
-    private void term() {
-        factor();
-        while(tokenList.get(index).getValue().equals("/")||
-                tokenList.get(index).getValue().equals("*")){
-            mulOp();
-            matchValue(tokenList.get(index));
-            factor();
-        }
-    }
-
-    private void addOp() {
-        if(tokenList.get(index).getValue().equals("+"))
-            matchValue(new Token("+"));
-        else if(tokenList.get(index).getValue().equals("-"))
-            matchValue(new Token("-"));
-        else
-            //TODO Handle Errors
-            return;
-    }
-    private void mulOp() {
-        if(tokenList.get(index).getValue().equals("*"))
-            matchValue(new Token("*"));
-        else if(tokenList.get(index).getValue().equals("/"))
-            matchValue(new Token("/"));
-        else
-            //TODO Handle Errors
-            return;
-    }
-    private void compOp() {
-        if(tokenList.get(index).getValue().equals("="))
-            matchValue(new Token("="));
-        else if(tokenList.get(index).getValue().equals("<"))
-            matchValue(new Token("<"));
-        else
-            //TODO Handle Errors
-            return;
-    }
 }
